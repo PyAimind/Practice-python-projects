62c1d0def08a1804540181295eefcf45d3807cb8
PyAimind <ali.pyaimind@gmail.com>
2025-10-19 17:27:17 +0330

    Add files via upload
    Create first python projects: circle calculator

---
+++ circle_projects.py
@@ -0,0 +1,8 @@
+import math
+r = int(input("please enter your radious:")) 
+p=math.pi
+# print(p)
+area = p * r ** 2
+print("\n area is:",area)
+perimeter = 2 * p * r
+print("\n perimeter is:",perimeter)
